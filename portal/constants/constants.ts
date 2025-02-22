6b92e44bff02f237649703dfb059295f07db08e3
Noel Luhanga <noeljayr01@gmail.com>
2025-02-22 22:36:23 +0200

    user profile

--- portal/constants/constants.ts
+++ portal/constants/constants.ts
@@ -1,7 +1,7 @@
 
 
-// const BASE_URL = "https://mzunipay-v2.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://mzunipay-v2.onrender.com/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 
 export { BASE_URL };
